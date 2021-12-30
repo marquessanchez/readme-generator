7e9c7e1838d5f342d52f4f2c848ef8e46ae8bacf
mrican217@gmail.com <mrican217@gmail.com>
2021-12-30 18:24:12 -0500

    first test created and passed

---
+++ Develop/README.md
@@ -0,0 +1,53 @@
+# po
+  ![Generic badge](https://img.shields.io/badge/license--blue.svg)
+
+  ## Description
+
+  p
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  #[Usage](#usage)
+
+  
+* [License](#license)
+
+  * [Contribution](#contribution)
+
+  *[Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installtion
+
+  To install necessary dependencies, run the following command:
+
+  '''
+  
+  '''
+
+  ## Usage
+
+  
+
+  ## License
+    
+    This project is licensed under the undefined license.
+
+  ## Contributing
+
+  
+
+  ## Tests
+
+  To run tests, run the following command:
+
+  '''
+  
+  '''
+
+  ## Questions 
+
+  If you have any questions about the repo, open an issue or contact me directly at .  You can find more of my work at [](https://github.com//).
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type:'input',
+    name: 'title',
+    message: 'What is the project title?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'What is the description of your project?'
+  },
+  {
+    type: 'input',
+    name: 'Table of Contents,',
+    message: 'What is the Table of Contents,?'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What is the Installation?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the Usage?'
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What is the License?'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Who is Contributing?'
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What is the Tests?'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is the Github username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is the email?'
+  }
+];
+
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    console.log(answers);
+  })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(),fileName),data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(res=>{
+    console.log('Generating README...');
+    writeToFile('README.md', generateMarkdown({ ...res}))
+  })
+}
 
 // Function call to initialize app
 init();
+
