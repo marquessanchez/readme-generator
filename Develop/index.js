df5f99668f7d5d126bc0d13d4c3860d4090c6ad6
mrican217@gmail.com <mrican217@gmail.com>
2022-03-17 22:31:30 -0400

    fix code

--- Develop/index.js
+++ Develop/index.js
@@ -64,12 +64,12 @@ inquirer
     console.log(answers);
   })
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(),fileName),data);
 }
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then(res=>{
     console.log('Generating README...');
